702748f9fb02faf01895fcc0664bb2e5968691cf
ibbiak <ibrahim.khaliq04@alumni.imperial.ac.uk>
2014-06-17 11:55:40 +0300

    Updated

--- DevelopingDataProduct/ui.R
+++ DevelopingDataProduct/ui.R
@@ -2,8 +2,8 @@
 ## Shiny Application - Monthly Mortgage Payment Calculator
 ## User Interface
 
-## This application takes the following inputs relating to the mortgae:
-## 1. The value of the mortgage / loan amoount
+## This application takes the following inputs relating to the mortgage:
+## 1. The value of the mortgage / loan amount
 ## 2. The interest rate for the mortgage
 ## 3. The term of the mortgage (in years)
 ## and outputs the expected monthly mortgage payments for an amortizing mortgage
@@ -12,6 +12,8 @@ library(shiny)
 shinyUI(pageWithSidebar(
         headerPanel("Monthly Amortizing Mortgage Payment Calculator"),
         sidebarPanel(
+                h6("Please enter the Mortgage Amount, Interest Rate and Term of the Mortgage in the input boxes provided below"),
+                h6("The inputs will be used to calculate your monthly mortgage payments after you click the Submit button"),
                 numericInput("Value", "Mortgage Amount", 0, min = 0),
                 numericInput("rate", label = "Annual Interest Rate in %", 0, min =0),
                 numericInput("Term", "Mortgage Term in Years", 0, min = 0),
